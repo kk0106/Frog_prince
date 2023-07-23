0179d8a06ac85e817875887ac1fe17e6587ca943
kai chang lin <kevin910106@gmail.com>
2023-07-23 15:52:30 +0800

    1

---
+++ Assets/player/UserInput.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    [HideInInspector] public Controls controls;
+    [HideInInspector] public Vector2 moveInput;
+    public static UserInput instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        controls = new Controls();
+        controls.playerControls.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
+
