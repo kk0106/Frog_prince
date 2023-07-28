637518b642640171107b91040477bb0a1b796cc9
Jasonchan832 <hy.chantab6656@gmail.com>
2023-07-29 04:17:33 +0800

    Revert "0728"
    This reverts commit f399233a912372c52be6fd23b9319eab790202ba.

--- Assets/camera/CameraFollowObject.cs
+++ Assets/camera/CameraFollowObject.cs
@@ -40,7 +40,7 @@ public class CameraFollowObject : MonoBehaviour
     {
         float startRotation = transform.localEulerAngles.y;
         float endRotation = DetermineEndRotation();
-        float yRotation = 10f;
+        float yRotation = 0f;
 
         float elapsedTime = 0f;
         while (elapsedTime < endRotation)
