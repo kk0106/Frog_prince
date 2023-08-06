ff13b2ed1d048844bef8cb0d9bb594f4140da037
kai chang lin <kevin910106@gmail.com>
2023-08-06 22:26:48 +0800

    0806(3)

--- Assets/T0_NPC/三隻小豬/script/talk.cs
+++ Assets/T0_NPC/三隻小豬/script/talk.cs
@@ -3,43 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class gototalk : MonoBehaviour
+public class talk : MonoBehaviour
 {
     public GameObject a;
-    public GameObject b;
+    public GameObject c;
     // Start is called before the first frame update
     void Start()
     {
         a.SetActive(false);
-        b.SetActive(false);
+        c.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (a.activeInHierarchy||b.activeInHierarchy)
+        if (a.activeInHierarchy || c.activeInHierarchy)
         {
             if (UserInput.instance.controls.playerControls.talk.WasPressedThisFrame())
             {
-                SceneManager.LoadScene(3);
+                SceneManager.LoadScene(4);
             }
         }
     }
-
     private void OnCollisionEnter(Collision other)
     {
         other.gameObject.tag = "Player";
         a.SetActive(true);
-        b.SetActive(true);
-        
+        c.SetActive(true);
+
     }
 
     private void OnCollisionExit(Collision other)
     {
         other.gameObject.tag = "Player";
         a.SetActive(false);
-            b.SetActive(false);
+        c.SetActive(false);
     }
-
-    
 }
