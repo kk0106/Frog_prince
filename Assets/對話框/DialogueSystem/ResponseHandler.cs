efb71f5afd67826d600cbcce1a7b98ce691c2d5b
Jasonchan832 <hy.chantab6656@gmail.com>
2023-10-11 08:44:01 +0800

    1011

--- Assets/對話框/DialogueSystem/ResponseHandler.cs
+++ Assets/對話框/DialogueSystem/ResponseHandler.cs
@@ -1,7 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class ResponseHandler : MonoBehaviour
 {
@@ -28,37 +28,36 @@ public class ResponseHandler : MonoBehaviour
     {
         float responseBoxHeight = 0;
 
-        for(int i = 0; i< responses.Length; i++)            
+        for (int i = 0; i < responses.Length; i++)
         {
             Response response = responses[i];
             int responseIndex = i;
-            
+
             GameObject responseButton = Instantiate(responseButtonTemplate.gameObject, responseContainer);
             responseButton.gameObject.SetActive(true);
             responseButton.GetComponent<TMP_Text>().text = response.ResponseText;
             responseButton.GetComponent<Button>().onClick.AddListener(() => OnPickedResponse(response, responseIndex));
 
-
+            tempResponseButtons.Add(responseButton);
 
             responseBoxHeight += responseButtonTemplate.sizeDelta.y;
         }
 
         responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x, responseBoxHeight);
         responseBox.gameObject.SetActive(true);
-
     }
 
     private void OnPickedResponse(Response response, int responseIndex)
     {
         responseBox.gameObject.SetActive(false);
 
-        foreach(GameObject button in tempResponseButtons)
+        foreach (GameObject button in tempResponseButtons)
         {
             Destroy(button);
         }
         tempResponseButtons.Clear();
-        
-        if(responseEvents != null && responseIndex <= responseEvents.Length)
+
+        if (responseEvents != null && responseIndex <= responseEvents.Length)
         {
             responseEvents[responseIndex].OnPickedResponse?.Invoke();
         }
