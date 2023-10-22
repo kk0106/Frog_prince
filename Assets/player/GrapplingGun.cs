ca02369d8e68f136d88aa438ce62043fe8cecbfa
Jasonchan832 <hy.chantab6656@gmail.com>
2023-10-22 17:00:17 +0800

    1022(2)

--- Assets/player/GrapplingGun.cs
+++ Assets/player/GrapplingGun.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using UnityEditor;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GrapplingGun : MonoBehaviour
@@ -15,20 +14,17 @@ public class GrapplingGun : MonoBehaviour
     public Transform gunTip, player;
     private Rigidbody playerRigidbody;
 
-
     private float maxDistance = 1.5f;
-    private float grappleRadius = 1.0f; // Adjust this radius to your needs
+    public float grappleRadius = 1.0f; // Adjust this radius to your needs
     private SpringJoint joint;
     public float damageAmount = 5f;
     public static int IsSwinging;
-    int numberOfRays = 18; // Adjust the number of rays as needed
-    float coneAngle = 0f; // Adjust the cone angle as needed
-
 
     private void Start()
     {
         playerRigidbody = player.GetComponent<Rigidbody>();
     }
+
     void Awake()
     {
         lr = GetComponent<LineRenderer>();
@@ -38,7 +34,6 @@ public class GrapplingGun : MonoBehaviour
     {
         if (UserInput.instance.controls.playerControls.GrappleGun.WasPressedThisFrame())
         {
-            
             StartGrapple();
             grappleCheck = 1;
         }
@@ -49,110 +44,78 @@ public class GrapplingGun : MonoBehaviour
         }
         else if (IsGrappling())
         {
-            DrawRope(); // Draw the rope and continue applying damage here
+            DrawRope();
         }
     }
+
     // Called after Update
     void LateUpdate()
     {
         DrawRope();
-
-        // Check if the player is retracting the grapple
-        if (UserInput.instance.controls.playerControls.GrappleGun.WasReleasedThisFrame() && IsGrappling())
+        if (UserInput.instance.controls.playerControls.GrappleGun.WasReleasedThisFrame() )
         {
             ApplyDamageToBreakableObject();
         }
         if (IsGrappling())
         {
-            // Apply gravity to the player's rigidbody while grappling
             playerRigidbody.velocity += Physics.gravity * Time.deltaTime;
         }
-
     }
 
-    // Call this method to apply damage to the breakable object
     void ApplyDamageToBreakableObject()
     {
-        if (IsGrappling() && breakableObject != null)
+        if (breakableObject == null)
         {
-            // Apply damage to the breakable object
+            return;
+        }
+        else
+        {
             breakableObject.ApplyDamage(damageAmount);
-
-            // Log a message to the console
-            Debug.Log("Damage Applied: " + damageAmount);
         }
     }
 
-    /// <summary>
-    /// Call whenever we want to start a grapple
-    /// </summary>
     void StartGrapple()
     {
-       
-        for (int i = 0; i < numberOfRays; i++)
+        Collider[] hitColliders = Physics.OverlapSphere(gunTip.position, grappleRadius, whatIsGrappleable);
+
+        foreach (var hitCollider in hitColliders)
         {
-            float angle = (-coneAngle / 2f) + (i * (coneAngle / (float)(numberOfRays - 1)));
-            Vector3 rayDirection = Quaternion.Euler(0, angle, 0) * gunTip.forward;
+            
+            // Get the closest point on the detected object
+            grapplePoint = hitCollider.ClosestPoint(gunTip.position);
 
-            if (Physics.SphereCast(gunTip.position, grappleRadius, rayDirection, out RaycastHit hit, maxDistance, whatIsGrappleable))
-            {
-                grapplePoint = hit.point;
-                // Rest of the grapple logic...
-                SetupGrapple();
-                IsSwinging = 1;
-                break; // Break out of the loop if a grapple point is found
-            }
-        }
-        
-       
-    }
+            // Set up the grapple without checking if it's breakable
+            SetupGrapple();
 
-    /// <summary>
-    /// Set up the SpringJoint and LineRenderer for grappling
-    /// </summary>
-    void SetupGrapple()
-    {
-        
-        RaycastHit hit;
-        // Use your existing SphereCast here
-        if (Physics.SphereCast(gunTip.position, grappleRadius, gunTip.forward, out hit, maxDistance, whatIsGrappleable))
-        {
-            // Attempt to find a BreakableObject component on the collided object
-            BreakableObject collidedObject = hit.collider.GetComponent<BreakableObject>();
-
+            // Indicate that you are swinging
+            IsSwinging = 1;
+            
+            BreakableObject collidedObject = hitCollider.GetComponent<BreakableObject>();
             if (collidedObject != null)
             {
                 breakableObject = collidedObject;
             }
-            else
-            {
-                // If the collided object doesn't have a BreakableObject component, clear the reference
-                breakableObject = null;
-            }
 
-            grapplePoint = hit.point;
-
-            joint = player.gameObject.AddComponent<SpringJoint>();
-            joint.autoConfigureConnectedAnchor = false;
-            joint.connectedAnchor = grapplePoint;
-
-            
-            // Set up SpringJoint properties (adjust these as needed)
-            joint.maxDistance = 1f;
-            joint.minDistance = 0.5f;
-            joint.spring = 3f;
-            joint.damper = 7f;
-            joint.massScale = 50f;
-
-            lr.positionCount = 2;
+                // Break out of the loop after grappling to the first valid object
+           break;
         }
     }
+    void SetupGrapple()
+    {
+        joint = player.gameObject.AddComponent<SpringJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = grapplePoint;
 
+        // Set up SpringJoint properties (adjust these as needed)
+        joint.maxDistance = 1f;
+        joint.minDistance = 0.5f;
+        joint.spring = 3f;
+        joint.damper = 7f;
+        joint.massScale = 50f;
 
+        lr.positionCount = 2;
+    }
 
-    /// <summary>
-    /// Call whenever we want to stop a grapple
-    /// </summary>
     void StopGrapple()
     {
         lr.positionCount = 0;
@@ -164,36 +127,14 @@ public class GrapplingGun : MonoBehaviour
 
     void DrawRope()
     {
-        
-        // If not grappling or the joint is null, don't draw rope
         if (!IsGrappling() || joint == null)
         {
-            // Reset the breakableObject reference to null
-            breakableObject = null;
             return;
         }
 
         currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
-
         lr.SetPosition(0, gunTip.position);
         lr.SetPosition(1, currentGrapplePosition);
-
-        // Check if we have a reference to the breakable object
-        if (breakableObject == null)
-        {
-            // Attempt to find the connected breakable object
-            if (joint.connectedBody != null)
-            {
-                breakableObject = joint.connectedBody.GetComponent<BreakableObject>();
-            }
-        }
-
-        // Ensure the breakableObject is not null before applying damage
-        if (breakableObject != null)
-        {
-            // Adjust damageAmount as needed
-            breakableObject.ApplyDamage(damageAmount);
-        }
     }
 
     public bool IsGrappling()
