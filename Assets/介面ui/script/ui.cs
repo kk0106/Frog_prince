2d9bf9775f1e95dc329c392da8251ae0885da72b
Jasonchan832 <hy.chantab6656@gmail.com>
2023-10-30 22:11:18 +0800

    1030(1)

--- Assets/介面ui/script/ui.cs
+++ Assets/介面ui/script/ui.cs
@@ -6,7 +6,7 @@ public class ui : MonoBehaviour
 {
     public GameObject hint;
     public GameObject hintText;
-    
+
     //public GameObject move_hint;
     //public GameObject jump_hint;
     //public GameObject talk_hint;
@@ -28,33 +28,26 @@ public class ui : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
-      
+        if (other.CompareTag("Player"))
+        {
             hintText.SetActive(true);
             hint.SetActive(true);
-
-            // move_hint.SetActive(true);
-            // jump_hint.SetActive(true);
-            //talk_hint.SetActive(true);
-            //swing_hint.SetActive(true);
-            //jumphook_hint.SetActive(true);
-        
+            // Additional hints or actions when the player enters
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.CompareTag("Player"))
+        {
             hintText.SetActive(false);
             hint.SetActive(false);
-
-            //move_hint.SetActive(false);
-            //jump_hint.SetActive(false);
-            // talk_hint.SetActive(false);
-            // swing_hint.SetActive(false);
-            //  jumphook_hint.SetActive(false);
-        
+            // Additional hints or actions when the player exits
+        }
     }
 }
