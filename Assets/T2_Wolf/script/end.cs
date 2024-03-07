28f46e6d5b55eaa72e14bb109220d8e08ae25987
kai chang lin <kevin910106@gmail.com>
2024-03-08 02:02:50 +0800

    0308(2)

--- Assets/T2_Wolf/script/end.cs
+++ Assets/T2_Wolf/script/end.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class end : MonoBehaviour
 {
+    public GameObject panel;
+    public float time;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        panel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (panel.activeInHierarchy)
+        {
+            time += Time.deltaTime;
+        }
+
+        if(time>4)
+        {
+            SceneManager.LoadScene("Start");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="wolf")
+        {
+            panel.SetActive(true);
+        }
     }
 }
