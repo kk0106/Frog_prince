b65027bbdede2dbfcceefde9413f2a652bf13d2d
Jasonchan832 <hy.chantab6656@gmail.com>
2024-03-07 09:34:49 +0800

    37

--- Assets/audio/audio_play.cs
+++ Assets/audio/audio_play.cs
@@ -12,11 +12,6 @@ public class audio_play : MonoBehaviour
         aud = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void play_sound()
     { 
         aud.Play();
