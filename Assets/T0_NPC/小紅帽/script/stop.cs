c44401c146420eb519c5f562b0f823d9638daaeb
kai chang lin <kevin910106@gmail.com>
2024-04-11 20:23:17 +0800

    0411(1)

--- Assets/T0_NPC/小紅帽/script/stop.cs
+++ Assets/T0_NPC/小紅帽/script/stop.cs
@@ -17,8 +17,8 @@ public class stop : MonoBehaviour
     {
         if(A.activeInHierarchy)
         {
-            player.MoveSpeed = 0f;
-            player.JumpForce = 0f;
+            //player.MoveSpeed = 0f;
+          //  player.JumpForce = 0f;
             
         }
     }
